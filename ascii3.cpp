722a5dc0b4e971f0da653d71c32aa136b2c767c6
Pratibha Mishra <mishh.pratibhaa@gmail.com>
2025-12-03 11:29:49 +0530

    Add files via upload

---
+++ ascii3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main() {
+    cout << "Enter ASCII values (0-255) separated by spaces:\n";
+
+    string input;
+    getline(cin, input);
+
+    stringstream ss(input);
+    int value;
+    string result = "";
+
+    while (ss >> value) {
+        if (value < 0 || value > 255) {
+            cout << "[Skipped invalid: " << value << "] ";
+            continue;
+        }
+        result += char(value);
+    }
+
+    cout << "Decoded Text: " << result << endl;
+    return 0;
+}
